9562836426cd696e851859298ae1de4beef94e34
Son Do <dominhson580@gmail.com>
2022-11-21 22:46:09 +0700

    Display 'all product' when category is undefined

--- src/components/List.js
+++ src/components/List.js
@@ -2,8 +2,10 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function List() {
-  const { category } = useParams("category");
-  console.log(category);
+  let { category } = useParams("category");
+  if (category === undefined) {
+    category = "all product";
+  }
 
   return <div>{category}</div>;
 }
