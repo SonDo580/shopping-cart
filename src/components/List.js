5ad7a08a32087dfe386794eebe6dc944daf89073
Son Do <dominhson580@gmail.com>
2022-11-21 22:27:20 +0700

    Use useParams to get URL parameter 'category'

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function List() {
-  return <div>List</div>;
+  const { category } = useParams("category");
+
+  return <div>{category}</div>;
 }
