929df38252003816b5faab14610ec2b6499c9f7a
Son Do <dominhson580@gmail.com>
2022-11-21 23:43:29 +0700

    Define the Product card

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function Product() {
-  return <div>Product</div>;
+export default function Product(props) {
+  const { product } = props;
+  return (
+    <div>
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.price}</p>
+      <p>
+        <button>-</button>
+        <span>1</span>
+        <button>+</button>
+      </p>
+      <button>Add to cart</button>
+      <hr />
+    </div>
+  );
 }
