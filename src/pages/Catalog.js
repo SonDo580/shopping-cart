97595dea111dca75dae65800441692b938596b14
Son Do <dominhson580@gmail.com>
2022-11-21 22:24:35 +0700

    Use Outlet

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,12 +1,12 @@
 import React from "react";
-import List from "../components/List";
+import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
 
 export default function Catalog() {
   return (
     <div style={{ display: "grid", gridTemplateColumns: "1fr 3fr" }}>
       <SideBar />
-      <List />
+      <Outlet />
     </div>
   );
 }
