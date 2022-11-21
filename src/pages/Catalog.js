532b623ca75c07c2ddd010daf6e246feff2aec64
Son Do <dominhson580@gmail.com>
2022-11-21 23:11:36 +0700

    Add price property

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -3,12 +3,12 @@ import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
 
 const products = [
-  { id: 1, category: "reptile", name: "t-rex" },
-  { id: 2, category: "mammal", name: "leopard" },
-  { id: 3, category: "insect", name: "dragonfly" },
-  { id: 4, category: "reptile", name: "spinosaurous" },
-  { id: 5, category: "mammal", name: "tiger" },
-  { id: 6, category: "insect", name: "mosquito" },
+  { id: 1, category: "reptile", name: "t-rex", price: "$100" },
+  { id: 2, category: "mammal", name: "leopard", price: "$120" },
+  { id: 3, category: "insect", name: "dragonfly", price: "$110" },
+  { id: 4, category: "reptile", name: "spinosaurous", price: "$200" },
+  { id: 5, category: "mammal", name: "tiger", price: "$150" },
+  { id: 6, category: "insect", name: "mosquito", price: "$180" },
 ];
 
 export default function Catalog() {
