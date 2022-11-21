8e310556ddf711aaf30f5e387901562f387899a0
Son Do <dominhson580@gmail.com>
2022-11-21 22:01:38 +0700

    Add sidebar and main section (list)

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,5 +1,12 @@
 import React from "react";
+import List from "../components/List";
+import SideBar from "../components/SideBar";
 
 export default function Catalog() {
-  return <div>Catalog</div>;
+  return (
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 3fr" }}>
+      <SideBar />
+      <List />
+    </div>
+  );
 }
