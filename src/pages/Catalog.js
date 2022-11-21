b72a8e9c22e4ad95e5bc500ab7ad868042fb1f34
Son Do <dominhson580@gmail.com>
2022-11-21 23:09:59 +0700

    Create a product list

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
 
+const products = [
+  { id: 1, category: "reptile", name: "t-rex" },
+  { id: 2, category: "mammal", name: "leopard" },
+  { id: 3, category: "insect", name: "dragonfly" },
+  { id: 4, category: "reptile", name: "spinosaurous" },
+  { id: 5, category: "mammal", name: "tiger" },
+  { id: 6, category: "insect", name: "mosquito" },
+];
+
 export default function Catalog() {
   return (
     <div style={{ display: "grid", gridTemplateColumns: "1fr 3fr" }}>
