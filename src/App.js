39eae26616864994701638191e51bedc820ed7a0
Son Do <dominhson580@gmail.com>
2022-11-21 16:49:38 +0700

    Define routes to 2 pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import NavBar from "./components/NavBar";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
