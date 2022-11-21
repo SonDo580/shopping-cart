b11a601d83458725d39637d20d4d68bb313f15b1
Son Do <dominhson580@gmail.com>
2022-11-21 22:18:22 +0700

    Add link to side bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SideBar() {
-  return <div>SideBar</div>;
+  return (
+    <aside>
+      <ul>
+        <li>
+          <Link to="/">All Robots</Link>
+        </li>
+        <li>
+          <Link to="reptile">Reptile</Link>
+        </li>
+        <li>
+          <Link to="mammal">Mammal</Link>
+        </li>
+        <li>
+          <Link to="insect">Insect</Link>
+        </li>
+      </ul>
+    </aside>
+  );
 }
