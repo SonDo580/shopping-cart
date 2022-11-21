3ef1c8ef15210eca31fb5eab744c40e8bb2523e8
Son Do <dominhson580@gmail.com>
2022-11-21 23:20:07 +0700

    Move products list to List.js

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,54 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
-import leopardImg from "../images/leopard.jpeg";
-import dinosaurImg from "../images/dinosaur.jpeg";
-import mosquitoImg from "../images/mosquito.jpeg";
-
-const products = [
-  {
-    id: 1,
-    category: "reptile",
-    name: "t-rex",
-    price: "$100",
-    image: dinosaurImg,
-  },
-  {
-    id: 2,
-    category: "mammal",
-    name: "leopard",
-    price: "$120",
-    image: leopardImg,
-  },
-  {
-    id: 3,
-    category: "insect",
-    name: "dragonfly",
-    price: "$110",
-    image: mosquitoImg,
-  },
-  {
-    id: 4,
-    category: "reptile",
-    name: "spinosaurous",
-    price: "$200",
-    image: dinosaurImg,
-  },
-  {
-    id: 5,
-    category: "mammal",
-    name: "tiger",
-    price: "$150",
-    image: dinosaurImg,
-  },
-  {
-    id: 6,
-    category: "insect",
-    name: "mosquito",
-    price: "$180",
-    image: mosquitoImg,
-  },
-];
 
 export default function Catalog() {
   return (
