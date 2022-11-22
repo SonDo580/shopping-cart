52ee0d77860d09a459d3b4781a1de078ec826fc3
Son Do <dominhson580@gmail.com>
2022-11-22 16:07:11 +0700

    Render chosen items and pass 'remove' function

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,17 @@
 import React from "react";
+import CartItem from "./CartItem";
 
-export default function Cart() {
-  return <div>Cart</div>;
+export default function Cart(props) {
+  const { chosenProducts, removeFromCart } = props;
+
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      <div className="cartItems">
+        {chosenProducts.map((item) => {
+          return <CartItem removeFromCart={removeFromCart} />;
+        })}
+      </div>
+    </div>
+  );
 }
