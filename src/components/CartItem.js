c202847918ca65cb95901c94204b683f938b263f
Son Do <dominhson580@gmail.com>
2022-11-22 16:33:43 +0700

    Create CartItem (similar to Product)

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function CartItem() {
-  return <div>CartItem</div>;
+export default function CartItem(props) {
+  const { item, removeFromCart } = props;
+
+  const [total, setTotal] = useState(item.total);
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  const changeQuantity = (amount) => {
+    if (quantity === 1 && amount === -1) {
+      return;
+    }
+    setQuantity((prevQuantity) => prevQuantity + amount);
+    setTotal((prevTotal) => prevTotal + amount * item.price);
+  };
+
+  return (
+    <div>
+      <img src={item.image} alt={item.name} />
+      <h3>{item.name}</h3>
+      <p>{`${item.unit}${total}`}</p>
+      <p>
+        <button onClick={() => changeQuantity(-1)}>-</button>
+        <span>{quantity}</span>
+        <button onClick={() => changeQuantity(1)}>+</button>
+      </p>
+      <button
+        onClick={() => {
+          removeFromCart();
+        }}
+      >
+        Remove from cart
+      </button>
+      <hr />
+    </div>
+  );
 }
