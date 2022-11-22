5157c523cf4e32512e550fa1c7b2898a93d29112
Son Do <dominhson580@gmail.com>
2022-11-22 14:30:33 +0700

    Add link to catalog page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <h1>Optimus Prime</h1>
       <p>Now available for preorder</p>
-      <button>Shop Now</button>
+      <Link to="/catalog">Shop Now</Link>
     </div>
   );
 }
