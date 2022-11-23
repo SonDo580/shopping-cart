4ca5586a10561534601c46798b896d3f84c7b89b
Son Do <dominhson580@gmail.com>
2022-11-23 08:49:18 +0700

    Modify function name

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,7 +6,7 @@ export default function CartItem(props) {
   const [total, setTotal] = useState(item.price);
   const [quantity, setQuantity] = useState(1);
 
-  const changeQuantity = (amount) => {
+  const changeQuantityByOne = (amount) => {
     if (quantity === 1 && amount === -1) {
       return;
     }
@@ -20,9 +20,9 @@ export default function CartItem(props) {
       <h3>{item.name}</h3>
       <p>{`${item.unit}${total}`}</p>
       <p>
-        <button onClick={() => changeQuantity(-1)}>-</button>
+        <button onClick={() => changeQuantityByOne(-1)}>-</button>
         <input value={quantity} />
-        <button onClick={() => changeQuantity(1)}>+</button>
+        <button onClick={() => changeQuantityByOne(1)}>+</button>
       </p>
       <button
         onClick={() => {
