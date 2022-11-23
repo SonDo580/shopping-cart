2be1de650a2a3123277ef44396489061f4592dff
Son Do <dominhson580@gmail.com>
2022-11-23 08:38:20 +0700

    Remove change quantity function
    Use it in CartItem

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,29 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Product(props) {
   const { chosenItems, product, addToCart } = props;
-
-  const [total, setTotal] = useState(product.price);
-  const [quantity, setQuantity] = useState(1);
-
-  const changeQuantity = (amount) => {
-    if (quantity === 1 && amount === -1) {
-      return;
-    }
-    setQuantity((prevQuantity) => prevQuantity + amount);
-    setTotal((prevTotal) => prevTotal + amount * product.price);
-  };
 
   return (
     <div>
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
-      <p>{`${product.unit}${total}`}</p>
-      <p>
-        <button onClick={() => changeQuantity(-1)}>-</button>
-        <span>{quantity}</span>
-        <button onClick={() => changeQuantity(1)}>+</button>
-      </p>
+      <p>{`${product.unit}${product.price}`}</p>
       <button
         onClick={() => {
           for (let item of chosenItems) {
