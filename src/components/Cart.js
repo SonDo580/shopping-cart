32a41ad1a1186415a6b48b5f00d3373e3fd2902f
Son Do <dominhson580@gmail.com>
2022-11-23 15:40:31 +0700

    Add 'total' to state

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "./CartItem";
 
 export default function Cart(props) {
   const { chosenItems, removeFromCart } = props;
+  const [total, setTotal] = useState((chosenItems) =>
+    chosenItems.reduce((sum, item) => {
+      return sum + item.price;
+    }, 0)
+  );
 
   return (
     <div>
       <div className="head">
         <h1>Your Cart</h1>
-        <strong>Total: {}</strong>
+        <strong>Total: {total}</strong>
         <button>Continue buying</button>
         <button>Check out</button>
       </div>
