b05859afcbf2e998ae31d2d5ccb54511c73c673f
Son Do <dominhson580@gmail.com>
2022-11-23 16:35:21 +0700

    Change 'total' when add new item to cart

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App() {
 
   const addToCart = (item) => {
     setChosenItems((prevChosenItems) => [...prevChosenItems, item]);
+    setTotal((prevTotal) => prevTotal + item.price);
   };
 
   const removeFromCart = (deleteID) => {
