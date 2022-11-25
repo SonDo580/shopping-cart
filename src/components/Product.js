3dc5ed57898dd4c6ad633e46b9ea110017f6dd35
Son Do <dominhson580@gmail.com>
2022-11-25 14:55:00 +0700

    Render remove or add button based on 'chosen'

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,20 +10,26 @@ export default function Product(props) {
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <p>{`$${product.price}`}</p>
-      <button
-        onClick={() => {
-          for (let chosenItem of chosenItems) {
-            if (chosenItem.id === product.id) {
-              alert("You already chose this product!");
-              return;
-            }
-          }
-          const item = { ...product, quantity: 1 };
-          addToCart(item);
-        }}
-      >
-        Add to cart
-      </button>
+
+      {chosen ? (
+        <button
+          onClick={() => {
+            setChosen(false);
+          }}
+        >
+          Remove from cart
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            setChosen(true);
+            const item = { ...product, quantity: 1 };
+            addToCart(item);
+          }}
+        >
+          Add to cart
+        </button>
+      )}
       <hr />
     </div>
   );
