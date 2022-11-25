688c2349fd7a38ea2b0e46c4db53c0d6ec23011c
Son Do <dominhson580@gmail.com>
2022-11-25 16:41:57 +0700

    Conditionally render the buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Product(props) {
-  const { product, addToCart, chosenItems } = props;
+  const { product, addToCart, removeFromCart } = props;
 
   return (
     <div>
@@ -9,14 +9,24 @@ export default function Product(props) {
       <h3>{product.name}</h3>
       <p>{`$${product.price}`}</p>
 
-      <button
-        onClick={() => {
-          const item = { ...product, quantity: 1 };
-          addToCart(item);
-        }}
-      >
-        Add to cart
-      </button>
+      {true ? (
+        <button
+          onClick={() => {
+            removeFromCart(product.id);
+          }}
+        >
+          Remove from cart
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            const item = { ...product, quantity: 1 };
+            addToCart(item);
+          }}
+        >
+          Add to cart
+        </button>
+      )}
       <hr />
     </div>
   );
