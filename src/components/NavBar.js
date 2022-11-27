ea7c19f3066b7143af51e93b9e29829ea10656f1
Son Do <dominhson580@gmail.com>
2022-11-27 22:07:50 +0700

    Display the number of products in cart

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../css/NavBar.css";
 
-export default function NavBar() {
+export default function NavBar(props) {
+  const { numChosenItems } = props;
+
   return (
     <nav>
       <Link className="logo" to="/">
         Mega Robot
       </Link>
       <Link to="/catalog">Shop Now</Link>
-      <Link to="/cart">Go to Cart</Link>
+      <Link to="/cart">Go to Cart ({numChosenItems})</Link>
     </nav>
   );
 }
