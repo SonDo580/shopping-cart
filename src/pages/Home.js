42ab0ee0782f56629ffe738716e9738fac89f1c7
Son Do <dominhson580@gmail.com>
2022-11-27 22:56:18 +0700

    Add a robot image to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import robotImg from "../images/robot.png";
 import "../css/Home.css";
 
 export default function Home() {
   return (
     <div className="home">
-      <h1>Optimus Prime</h1>
-      <p>Now available for preorder</p>
-      <button>
-        <Link to="/catalog">Shop Now</Link>
-      </button>
+      <div className="left">
+        <h1>Optimus Prime</h1>
+        <p>Now available for preorder</p>
+        <button>
+          <Link to="/catalog">Shop Now</Link>
+        </button>
+      </div>
+      <div className="right">
+        <img src={robotImg} alt="main robot" />
+      </div>
     </div>
   );
 }
