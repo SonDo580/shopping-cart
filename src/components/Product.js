31d8ffa9a95a87e386bc9aee8dbfd4101543d3fa
Son Do <dominhson580@gmail.com>
2022-11-27 21:25:56 +0700

    Get index of the product in chosenItems

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,8 +3,11 @@ import React from "react";
 export default function Product(props) {
   const { product, addToCart, removeFromCart, chosenItems } = props;
 
-  const isChosen =
-    chosenItems.findIndex((item) => item.id === product.id) !== -1;
+  const indexChosen = chosenItems.findIndex((item) => item.id === product.id);
+
+  // if (indexChosen) {
+  //   const item = chosenItems
+  // }
 
   return (
     <div>
@@ -12,7 +15,7 @@ export default function Product(props) {
       <h3>{product.name}</h3>
       <p>{`$${product.price}`}</p>
 
-      {isChosen ? (
+      {indexChosen !== -1 ? (
         <button
           onClick={() => {
             removeFromCart(product.id);
