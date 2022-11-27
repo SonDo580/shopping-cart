2adc47b5e8026a3db5b8514e8c6549a21d8ddbc8
Son Do <dominhson580@gmail.com>
2022-11-27 22:49:08 +0700

    Style the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/Home.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>Optimus Prime</h1>
       <p>Now available for preorder</p>
-      <Link to="/catalog">Shop Now</Link>
+      <button>
+        <Link to="/catalog">Shop Now</Link>
+      </button>
     </div>
   );
 }
