dc9022bdb09d52e248d46c006fdf2519045746d0
Son Do <dominhson580@gmail.com>
2023-02-01 23:41:24 +0700

    Use the fake API

--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { Images } from "../../constants/images";
 
 const initialState = { isLoading: false, productList: [] };
 
@@ -19,59 +18,12 @@ export const productSlice = createSlice({
   },
 });
 
-const products = [
-  {
-    id: 1,
-    category: "reptile",
-    name: "t-rex",
-    price: 100,
-    image: Images.DINOSAUR,
-  },
-  {
-    id: 2,
-    category: "mammal",
-    name: "leopard",
-    price: 120,
-    image: Images.LEOPARD,
-  },
-  {
-    id: 3,
-    category: "insect",
-    name: "dragonfly",
-    price: 110,
-    image: Images.MOSQUITO,
-  },
-  {
-    id: 4,
-    category: "reptile",
-    name: "spinosaurous",
-    price: 200,
-    image: Images.DINOSAUR,
-  },
-  {
-    id: 5,
-    category: "mammal",
-    name: "tiger",
-    price: 150,
-    image: Images.LEOPARD,
-  },
-  {
-    id: 6,
-    category: "insect",
-    name: "mosquito",
-    price: 180,
-    image: Images.MOSQUITO,
-  },
-];
-
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
   async () => {
-    return await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(products);
-      }, 3000);
-    });
+    const res = await fetch("api/products");
+    const data = await res.json();
+    return data.products;
   }
 );
 
