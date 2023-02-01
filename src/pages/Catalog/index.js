62740651413e87dfa6efd24f9feb23bc0f1ab291
Son Do <dominhson580@gmail.com>
2023-02-01 23:09:13 +0700

    Show 'Loading...'

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,13 +1,27 @@
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import SideBar from "../../components/SideBar";
+import { loadingStatusSelector } from "../../redux/selectors";
 
 export default function Catalog() {
+  const isLoading = useSelector(loadingStatusSelector);
+
   return (
-    <div
-      style={{ display: "grid", gridTemplateColumns: "1fr 3fr", flexGrow: 1 }}
-    >
-      <SideBar />
-      <Outlet />
-    </div>
+    <>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 3fr",
+            flexGrow: 1,
+          }}
+        >
+          <SideBar />
+          <Outlet />
+        </div>
+      )}
+    </>
   );
 }
