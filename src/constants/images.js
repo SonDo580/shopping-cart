915d2e7f4ef59c5264d29508c4e419d827e1f33b
Son Do <dominhson580@gmail.com>
2023-02-01 15:04:01 +0700

    Store images in 1 file

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import DINOSAUR from "../images/dinosaur.jpeg";
+import LEOPARD from "../images/leopard.jpeg";
+import MOSQUITO from "../images/mosquito.jpeg";
+import ROBOT from "../images/robot.png";
+
+export const Images = {
+  DINOSAUR,
+  LEOPARD,
+  MOSQUITO,
+  ROBOT,
+};
