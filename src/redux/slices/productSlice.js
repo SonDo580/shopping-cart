464151c5c0ed6c1f077bd663067ec7240cc51c4d
Son Do <dominhson580@gmail.com>
2023-02-01 22:32:39 +0700

    Create thunk fetchTodos

--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { Images } from "../../constants/images";
 
 const initialState = { status: "idle", productList: [] };
 
@@ -7,5 +8,61 @@ export const productSlice = createSlice({
   initialState,
   reducers: {},
 });
+
+const products = [
+  {
+    id: 1,
+    category: "reptile",
+    name: "t-rex",
+    price: 100,
+    image: Images.DINOSAUR,
+  },
+  {
+    id: 2,
+    category: "mammal",
+    name: "leopard",
+    price: 120,
+    image: Images.LEOPARD,
+  },
+  {
+    id: 3,
+    category: "insect",
+    name: "dragonfly",
+    price: 110,
+    image: Images.MOSQUITO,
+  },
+  {
+    id: 4,
+    category: "reptile",
+    name: "spinosaurous",
+    price: 200,
+    image: Images.DINOSAUR,
+  },
+  {
+    id: 5,
+    category: "mammal",
+    name: "tiger",
+    price: 150,
+    image: Images.LEOPARD,
+  },
+  {
+    id: 6,
+    category: "insect",
+    name: "mosquito",
+    price: 180,
+    image: Images.MOSQUITO,
+  },
+];
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    return await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(products);
+      }, 2000);
+    });
+  }
+);
 
 export default productSlice.reducer;
