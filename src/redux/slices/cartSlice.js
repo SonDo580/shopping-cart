3a59fd931cd01eaa56cfa239c026cde825b06587
Son Do <dominhson580@gmail.com>
2023-02-01 17:57:24 +0700

    Change slice name

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [];
 
 export const cartSlice = createSlice({
-  name: "carts",
+  name: "cart",
   initialState,
   reducers: {},
 });
