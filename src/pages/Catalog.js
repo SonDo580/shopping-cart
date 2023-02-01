a55c2ca3409e5b2d02e38910715a3178d2e6f043
Son Do <dominhson580@gmail.com>
2023-02-01 15:48:52 +0700

    Make sidebar span the remaining height

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,9 +1,12 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
 
 export default function Catalog() {
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 3fr" }}>
+    <div
+      style={{ display: "grid", gridTemplateColumns: "1fr 3fr", flexGrow: 1 }}
+    >
       <SideBar />
       <Outlet />
     </div>
