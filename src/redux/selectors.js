efe99555f3b865c3335cef6c13a25f0be1df16b1
Son Do <dominhson580@gmail.com>
2023-02-02 21:46:51 +0700

    Create errorFetching selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,3 @@
 export const productListSelector = (state) => state.products.productList;
 export const loadingStatusSelector = (state) => state.products.isLoading;
+export const errorFetchingSelector = (state) => state.products.error;
