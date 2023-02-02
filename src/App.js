c458cd80cb9eda1429ebddaa4a2adb89d719de42
Son Do <dominhson580@gmail.com>
2023-02-02 23:30:13 +0700

    Remove redundant code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -21,79 +21,17 @@ export default function App() {
     dispatch(fetchProducts());
   }, []);
 
-  const [chosenItems, setChosenItems] = useState([]);
-
-  const [total, setTotal] = useState(0);
-
-  const addToCart = (item) => {
-    setChosenItems((prevChosenItems) => [...prevChosenItems, item]);
-    setTotal((prevTotal) => prevTotal + item.price);
-  };
-
-  const updateQuantity = (updateID, newQuantity, totalIncrease) => {
-    setChosenItems((prevChosenItems) => {
-      let newChosenItems = [...prevChosenItems];
-      let updateItem = newChosenItems.find((item) => item.id === updateID);
-      updateItem.quantity = newQuantity;
-      return newChosenItems;
-    });
-
-    setTotal((prevTotal) => prevTotal + totalIncrease);
-  };
-
-  const removeFromCart = (deleteID, totalRemove) => {
-    setChosenItems((prevChosenItems) => {
-      let deleteIndex = prevChosenItems.findIndex(
-        (item) => item.id === deleteID
-      );
-      return [
-        ...prevChosenItems.slice(0, deleteIndex),
-        ...prevChosenItems.slice(deleteIndex + 1),
-      ];
-    });
-
-    setTotal((prevTotal) => prevTotal - totalRemove);
-  };
-
   return (
     <div className="app">
       <BrowserRouter basename="/shopping-cart">
-        <NavBar numChosenItems={chosenItems.length} />
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/catalog" element={<Catalog />}>
-            <Route
-              path=""
-              element={
-                <ProductList
-                  addToCart={addToCart}
-                  removeFromCart={removeFromCart}
-                  chosenItems={chosenItems}
-                />
-              }
-            />
-            <Route
-              path=":category"
-              element={
-                <ProductList
-                  addToCart={addToCart}
-                  removeFromCart={removeFromCart}
-                  chosenItems={chosenItems}
-                />
-              }
-            />
+            <Route path="" element={<ProductList />} />
+            <Route path=":category" element={<ProductList />} />
           </Route>
-          <Route
-            path="/cart"
-            element={
-              <Cart
-                chosenItems={chosenItems}
-                removeFromCart={removeFromCart}
-                updateQuantity={updateQuantity}
-                total={total}
-              />
-            }
-          />
+          <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
       </BrowserRouter>
