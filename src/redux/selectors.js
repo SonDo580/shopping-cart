ca9720c6cc579f33ca443891521330d0995f6bdd
Son Do <dominhson580@gmail.com>
2023-02-02 22:50:27 +0700

    Add totalSelector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,13 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const productListSelector = (state) => state.products.productList;
 export const loadingStatusSelector = (state) => state.products.isLoading;
 export const errorFetchingSelector = (state) => state.products.error;
 
 export const cartItemsSelector = (state) => state.cart;
+export const totalSelector = createSelector(cartItemsSelector, (cartItems) => {
+  return cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+});
