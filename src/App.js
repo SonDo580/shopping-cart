dcb847f5bad2c83c0ac1e6c607fc6508710c1d1d
Son Do <dominhson580@gmail.com>
2023-02-03 08:44:00 +0700

    Use App.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
